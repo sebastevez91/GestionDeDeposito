15f56d17d6bdb7b62214f4abe2277ce568367def
Sebastian <sebastevez91@gmail.com>
2025-04-27 21:17:11 -0300

    Create Controllers

---
+++ GES-17/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        // Lógica de autenticación va acá
+        return Ok();
+    }
+}
+
