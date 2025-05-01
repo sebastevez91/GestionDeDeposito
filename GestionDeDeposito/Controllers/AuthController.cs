d2324f54ca030b6db1e1b80b5e5a5e3717183b9c
Sebastian <sebastevez91@gmail.com>
2025-05-01 16:33:52 -0300

    Update project

---
+++ GestionDeDeposito/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionDeDeposito.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            // Lógica de autenticación va acá
+            return Ok();
+        }
+    }
+}
+
---
+++ GestionDeDeposito/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionDeDeposito.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
