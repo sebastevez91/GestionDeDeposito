b578f211074bb212622c9b939f0627b879bab446
Sebastian <sebastevez91@gmail.com>
2025-06-14 18:30:07 -0300

    Modify connection db

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,29 @@
-const mysql = require('mysql2/promise');
+const sql = require('myssql');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10
-});
+const config = {
+  server: process.env.DB_SERVER, 
+  database: process.env.DB_NAME, // nombre de la base datos
+  user: process.env.DB_USER,     // nombre de usuario
+  password: process.env.DB_PASSWORD,  // contraseña
+  port: parseInt(process.env.DB_PORT) || 1433,
+  options: {
+    encrypt: true,    // necesario en muchas conexiones
+    trustServerCertificate: true,    //certificados autofirmados
+  }
+};
 
-module.exports = pool;
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Conectado a SQL Server');
+    return pool;
+  })
+  .catch(err => {
+    console.error('Error al conectar a SQL Server: ', err);
+  });
+
+module.exports = { 
+  sql,
+  poolPromise
+};
